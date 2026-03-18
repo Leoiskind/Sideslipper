6f9eb1a1341988bf77cfbb8538cbbe85acf85bce
f42971lm <leo.morgan@student.manchester.ac.uk>
2026-03-18 21:29:28 +0000

    finished inventory

--- main.py
+++ main.py
@@ -86,13 +86,54 @@ class Inventory(Entity):
 		self.item_parent = Entity(parent=self, scale=(1/5,1/8))
 
 	def append(self, item):
-		Button(
+		icon=Draggable(
 			parent = inventory.item_parent,
 			model = 'quad',
+			texture= item,
 			origin = (-.5,.5),
-			color = color.random_color(),
+			color = color.white,
+			position = self.find_free_spot(),
 			z = -.1
 			)
+		
+		def drag():
+			icon.org_pos=(icon.x, icon.y)
+			icon.z += .1
+
+		def drop():
+			icon.x = int(round(icon.x))
+			icon.y = int(round(icon.y))
+			icon.z = -.1
+
+			if icon.x<0 or icon.x>=5 or icon.y>0 or icon.y<-7:
+				print("out of bounds")
+				icon.position=(icon.org_pos)
+				icon.z= -.1
+				return
+
+			for c in self.item_parent.children:
+				if c == icon:
+					continue
+				if c.x==icon.x and c.y==icon.y:
+					print('swap positions')
+					c.position = icon.org_pos
+					c.z = -0.1
+
+
+		icon.drag=drag
+		icon.drop=drop
+		name=item.replace('_', '').title()
+		icon.tooltip=Tooltip(name)
+		icon.tooltip.background.color=color.hsv(0,0,0,.8)
+		
+	def find_free_spot(self):
+		taken_spots = [(int(e.x), int(e.y)) for e in self.item_parent.children]
+		for y in range(8):
+			for x in range(5):
+				if not (x, -y) in taken_spots:
+					return(x, -y)
+
+
 
 # Static camera behind the bean and looking toward origin (center of corridor)
 camera.position = Vec3(0, 0, 0)
@@ -159,4 +200,19 @@ def update():
 if __name__ == '__main__':
 	print('\nControls: A/D or left/right arrows = left/right wall. W/S or up/down arrows = ceiling/floor.')
 	print('The bean does not translate; corridor segments move to simulate running.\n')
+	inventory=Inventory()
+	
+	def add_item():
+		inventory.append(random.choice(['bag', 'car']))
+	
+	for i in range(7):
+		inventory.append('test item')
+	add_item_button = Button(
+		scale = (.1,.1),
+		x=-.5,
+		color=color.lime.tint(-.25),
+		text='+',
+		tooltip=Tooltip('Add random item'),
+		on_click=add_item
+	)
 	app.run()
