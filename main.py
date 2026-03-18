d75375479a344ad9d5652da5d5c459eabcaa508f
PSSP12 <pabotante@gmail.com>
2026-03-18 16:45:04 +0000

    My very small contribution

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Hello world")
+from ursina import *
+
+app = Ursina()
+
+# --- 2D style camera ---
+camera.orthographic = True
+camera.fov = 10
+window.color = color.black
+
+# --- Player ---
+player = Entity(
+    model='quad',
+    color=color.azure,
+    scale=(1,1),
+    collider='box'
+)
+
+speed = 5
+
+# --- Ground / reference ---
+ground = Entity(
+    model='quad',
+    scale=(20,10),
+    color=color.dark_gray,
+    z=1
+)
+
+# --- Game loop ---
+def update():
+    player.x += (held_keys['d'] - held_keys['a']) * speed * time.dt
+    player.y += (held_keys['w'] - held_keys['s']) * speed * time.dt
+
+app.run()
