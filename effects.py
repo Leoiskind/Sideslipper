fc7adc2266d122f98e2b351ac9a049de55aecee5
f42971lm <leo.morgan@student.manchester.ac.uk>
2026-03-23 17:21:34 +0000

    fixed fries

--- effects.py
+++ effects.py
@@ -156,7 +156,7 @@ class EffectsManager:
             self.extra_time += 2
             def reset_fries():
                 self.extra_time -= 2
-            invoke(reset_fries, delay=self.effect_time['fries'] + self.extra_time)
+            invoke(reset_fries, 2)
         elif item_name == 'apple':
             EffectTimer('apple', self.effect_time['apple'] + self.extra_time)
             self.lose_effects_callback()  # Clear all current effects immediately
